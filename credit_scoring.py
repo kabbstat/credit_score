20b55d0d713ab5d0be0cc66f1d8171010353df4d
kabbaj mohamed <kabbaj.mohamed95@gmail.com>
2025-04-18 11:31:33 +0100

    update

--- credit_scoring.py
+++ credit_scoring.py
@@ -74,9 +74,54 @@ for feature in critical_features:
 extracted = df['credit_history_age'].str.extract(r'(?P<Years>\d+)\s*Years\s*and\s*(?P<Months>\d+)\s*Months').astype(float)
 df['total_month'] = extracted['Years'] * 12 + extracted['Months']
 # dealing with missing values in the critical features
-df['total_month'].fillna(df['total_month'].meadian(), inplace=True)
+# credit_history_age
+df['total_month'].fillna(df['total_month'].median(), inplace=True)
 critical_features_preprocessing = Pipeline([('imputer', KNNImputer(n_neighbors=3, weights='distance')),
                                  'scaler', StandardScaler()])
+# Num_of_delayed_payment
+df['num_of_delayed_payment'].fillna(df['num_of_delayed_payment'].median(),inplace=True)
+# payment_behaviour
+df['payment_behaviuour']= df['paymnet_behaviour'].replace('!@9#%8','unknown')
+# ploting the distribution of the critical features with target variable 
+def plot_critical_features_cat_target(feature, title, hue=None, palette='Set2'):
+    plt.figure(figsize=(12, 7))  
+    ax = sns.countplot(   x=feature,   hue=hue,   data=df,  palette=palette,  edgecolor='black',   linewidth=1.2)
+    for p in ax.patches:
+        ax.annotate(
+            f'{p.get_height():,.0f}', 
+            (p.get_x() + p.get_width() / 2., p.get_height()),  ha='center',  va='bottom',  fontsize=10,   color='black', xytext=(0, 5), textcoords='offset points' )
+    plt.title(title, fontsize=16, pad=20)
+    plt.xlabel(feature, fontsize=14, labelpad=10)
+    plt.ylabel('Count', fontsize=14, labelpad=10)
+    plt.xticks(rotation=45, ha='right', fontsize=12)
+    if hue: plt.legend( title='Target Variable',title_fontsize=12,fontsize=11,frameon=True,framealpha=0.8, edgecolor='black' )
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.show()
+def plot_critical_features_num_target(feature, target, title, palette='Set2'):
+    plt.figure(figsize=(12, 7))
+    ax = sns.boxplot(
+        x=target, y=feature, data=df, hue=target,  palette=palette,showmeans=True,
+          meanprops={"marker":"o", "markerfacecolor":"white", "markersize":"10", "markeredgecolor":"black"},linewidth=1.5
+    )
+    plt.title(title, fontsize=16, pad=20)
+    plt.xlabel(target, fontsize=14, labelpad=10)
+    plt.ylabel(feature, fontsize=14, labelpad=10)
+    plt.xticks(rotation=45, ha='right', fontsize=12)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    if feature == 'num_of_delayed_payment':
+        plt.ylim(0, df[feature].quantile(0.95))  # Zoom sur 95% des données
+    legend = ax.get_legend()
+    if legend is not None:
+        legend.remove()  # Supprime la légende redondante
+    
+    plt.tight_layout()
+    plt.show()
+plot_critical_features_num_target('total_month', 'target', 'Distribution of credit_history_age with target variable')
+plot_critical_features_num_target('num_of_delayed_payment', 'target', 'Distribution of num_of_delayed_payment with target variable')
+plot_critical_features_cat_target('payment_behaviour', 'Distribution of payment_behaviour with target variable', hue='target')
+plot_critical_features_num_target('credit_utilization_ratio', 'target', 'Distribution of credit_utilization_ratio with target variable')
+plot_critical_features_num_target('outstanding_debt', 'target', 'Distribution of outstanding_debt with target variable')
 
 # checking the distribution of the critical features with target variable
 for feature in critical_features:
